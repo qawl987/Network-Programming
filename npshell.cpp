3f964091d5a90a742a1c00a31093ffd872a8c7b3
qawl987 <shiny861216@gmail.com>
2025-03-28 05:53:30 +0000

    feat: exec function and reap child

--- npshell.cpp
+++ npshell.cpp
@@ -1,6 +1,7 @@
 #include <ctype.h>
 #include <fcntl.h>
 #include <iostream>
+#include <memory>
 #include <queue>
 #include <sstream>
 #include <stdio.h>
@@ -15,7 +16,7 @@ using namespace std;
 
 void sigFork(int signo) {
     int status;
-    while (waitpid(-1, &status, WNOHANG)) {
+    while (waitpid(-1, &status, WNOHANG) > 0) {
     }
 }
 
@@ -23,56 +24,76 @@ struct Command {
     vector<string> args;
 };
 
-void parse(Command &c, string input) {
-    stringstream ss(input);
-    string arg;
-    while (getline(ss, arg, ' ')) {
-        cout << arg << endl;
-        if (arg[0] == '|') {
-            cout << "pipe" << endl;
-        } else if (arg[0] == '!') {
-            cout << "error" << endl;
-        } else if (arg[0] == '>') {
-            cout << "redirect" << endl;
-        } else {
-            cout << "command" << endl;
-            c.args.push_back(arg);
-        }
+void executeCommand(const Command &command) {
+    if (command.args[0] == "exit") {
+        exit(0);
     }
-    return;
-}
 
-void execute(Command &c) {
+    if (command.args[0] == "setenv") {
+        setenv(command.args[1].c_str(), command.args[2].c_str(), 1);
+        return;
+    }
+
+    if (command.args[0] == "printenv") {
+        if (const char *env = getenv(command.args[1].c_str())) {
+            cout << env << '\n';
+        }
+        return;
+    }
+
     pid_t child;
     while ((child = fork()) == -1) {
         if (errno == EAGAIN) {
             wait(nullptr); // wait for any child process to release resource
         }
     }
-
-    if (child != 0) { // parent process
-        // close pipe
+    // parent process
+    if (child != 0) {
+        // wait is important
+        waitpid(child, nullptr, 0);
         return;
     }
-    // auto args = make_unique<char *[]>(command.args.size() + 1);
-    // for (size_t i = 0; i < command.args.size(); i++) {
-    //     args[i] = strdup(command.args[i].c_str());
-    // }
-    // args[command.args.size()] = nullptr;
 
-    // if (execvp(args[0], args.get()) == -1 && errno == ENOENT) {
-    //     cerr << "Unknown command: [" << args[0] << "].\n";
-    //     exit(0);
-    // }
-    // for (auto &bin : c.binGroup) {
-    //     cout << bin.first << " " << bin.second << endl;
-    // }
+    // child process
+    vector<char *> args;
+    for (const auto &arg : command.args) {
+        args.push_back(strdup(arg.c_str()));
+    }
+    args.push_back(nullptr);
+    if (execvp(args[0], args.data()) == -1 && errno == ENOENT) {
+        cerr << "Unknown command: [" << args[0] << "].\n";
+        exit(0);
+    }
+    return;
 }
 
 void initShell() {
     clearenv();
     setenv("PATH", "bin:.", 1);
     signal(SIGCHLD, sigFork); // call wait() catch state
+}
+
+vector<Command> parseCommands(string input) {
+    vector<Command> commands;
+    vector<string> command_args;
+    stringstream ss(input);
+    string arg;
+    while (getline(ss, arg, ' ')) {
+        if (arg[0] == '|' || arg[0] == '!' || arg[0] == '>') {
+            Command command;
+            command.args = command_args;
+            command_args.clear();
+            commands.push_back(command);
+        } else {
+            command_args.push_back(arg);
+        }
+    }
+    if (!command_args.empty()) { // parse last command
+        Command command;
+        command.args = command_args;
+        commands.push_back(command);
+    }
+    return commands;
 }
 
 int main() {
@@ -84,16 +105,10 @@ int main() {
         if (input.empty()) {
             continue;
         }
-        Command c;
-        // If
-        vector<Command> commandList;
-        parse(c, input);
-        execute(c);
-        // for (auto &command : c.commands) {
-        //     cout << command << endl;
-        // }
-        // for (auto &arg : c.args) {
-        //     cout << arg << endl;
-        // }
+        vector<Command> commands = parseCommands(input);
+        for (auto &command : commands) {
+            executeCommand(command);
+        }
     }
+    return 0;
 }
