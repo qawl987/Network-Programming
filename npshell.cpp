2a1621bc82e3d58e69880213c2cd98a5a10a9ef6
qawl987 <shiny861216@gmail.com>
2025-03-28 12:44:50 +0000

    refactor: OO style code

--- npshell.cpp
+++ npshell.cpp
@@ -13,194 +13,266 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <unordered_map>
-#include <vector>
+
 using namespace std;
 
-void sigFork(int signo) {
-    int status;
-    while (waitpid(-1, &status, WNOHANG) > 0) {
-    }
-}
+class ProcessExecutor {
+  public:
+    struct ProcessConfig {
+        vector<string> arguments;
+        array<int, 2> input_pipe = {STDIN_FILENO, STDOUT_FILENO};
+        int output_fd = STDOUT_FILENO;
+        int error_fd = STDERR_FILENO;
+    };
 
-struct Command {
-    vector<string> args;
-    int fd_out = STDOUT_FILENO;
-    int fd_err = STDERR_FILENO;
-    // 0: fd_in, 1: fd write to fd_in
-    int pipe[2] = {STDIN_FILENO, STDOUT_FILENO};
+    static void run(const ProcessConfig &config) {
+        if (handleBuiltins(config)) {
+            return;
+        }
+
+        pid_t pid = createChildProcess();
+        if (pid != 0) {
+            cleanupParentResources(config);
+            waitForChildIfNeeded(pid, config);
+            return;
+        }
+
+        setupChildProcessIO(config);
+        executeExternalCommand(config);
+    }
+
+  private:
+    static bool handleBuiltins(const ProcessConfig &config) {
+        const string &cmd = config.arguments[0];
+
+        if (cmd == "exit") {
+            exit(0);
+        }
+
+        if (cmd == "setenv") {
+            setenv(config.arguments[1].c_str(), config.arguments[2].c_str(), 1);
+            return true;
+        }
+
+        if (cmd == "printenv") {
+            if (const char *env = getenv(config.arguments[1].c_str())) {
+                cout << env << '\n';
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+    static pid_t createChildProcess() {
+        pid_t pid;
+        while ((pid = fork()) == -1) {
+            if (errno == EAGAIN) {
+                wait(nullptr);
+            }
+        }
+        return pid;
+    }
+
+    static void cleanupParentResources(const ProcessConfig &config) {
+        if (config.input_pipe[0] != STDIN_FILENO) {
+            close(config.input_pipe[0]);
+            close(config.input_pipe[1]);
+        }
+
+        struct stat fd_stat;
+        fstat(config.output_fd, &fd_stat);
+
+        if (config.output_fd != STDOUT_FILENO && S_ISREG(fd_stat.st_mode)) {
+            close(config.output_fd);
+        }
+    }
+
+    static void waitForChildIfNeeded(pid_t pid, const ProcessConfig &config) {
+        struct stat fd_stat;
+        fstat(config.output_fd, &fd_stat);
+
+        if (!S_ISFIFO(fd_stat.st_mode)) {
+            waitpid(pid, nullptr, 0);
+        }
+    }
+
+    static void setupChildProcessIO(const ProcessConfig &config) {
+        dup2(config.input_pipe[0], STDIN_FILENO);
+        dup2(config.output_fd, STDOUT_FILENO);
+        dup2(config.error_fd, STDERR_FILENO);
+    }
+
+    static void executeExternalCommand(const ProcessConfig &config) {
+        auto argv = prepareCommandArguments(config);
+
+        if (execvp(argv[0], argv.get())) {
+            if (errno == ENOENT) {
+                cerr << "Unknown command: [" << argv[0] << "].\n";
+                exit(0);
+            }
+        }
+    }
+
+    static unique_ptr<char *[]>
+    prepareCommandArguments(const ProcessConfig &config) {
+        auto args = make_unique<char *[]>(config.arguments.size() + 1);
+        for (size_t i = 0; i < config.arguments.size(); i++) {
+            args[i] = strdup(config.arguments[i].c_str());
+        }
+        args[config.arguments.size()] = nullptr;
+        return args;
+    }
 };
 
-void executeCommand(const Command &command) {
-    if (command.args[0] == "exit") {
-        exit(0);
-    }
+class PipeManager {
+    unordered_map<int, array<int, 2>> active_pipes;
 
-    if (command.args[0] == "setenv") {
-        setenv(command.args[1].c_str(), command.args[2].c_str(), 1);
-        return;
-    }
-
-    if (command.args[0] == "printenv") {
-        if (const char *env = getenv(command.args[1].c_str())) {
-            cout << env << '\n';
-        }
-        return;
-    }
-
-    pid_t child;
-    while ((child = fork()) == -1) {
-        if (errno == EAGAIN) {
-            wait(nullptr); // wait for any child process to release resource
-        }
-    }
-    // parent process
-    if (child != 0) {
-        // close pipe
-        if (command.pipe[0] != STDIN_FILENO) {
-            close(command.pipe[0]);
-            close(command.pipe[1]);
-        }
-        // wait is important
-        struct stat fd_stat;
-        fstat(command.fd_out, &fd_stat);
-        // close file if fd_out isn't STDOUT_FILENO
-        if (command.fd_out != STDOUT_FILENO && S_ISREG(fd_stat.st_mode)) {
-            close(command.fd_out);
-        }
-        // wait for child when fd_out isn't pipe
-        if (!S_ISFIFO(fd_stat.st_mode)) {
-            waitpid(child, nullptr, 0);
-        }
-        return;
-    }
-
-    // child process
-    dup2(command.pipe[0], STDIN_FILENO);
-    dup2(command.fd_out, STDOUT_FILENO);
-    dup2(command.fd_err, STDERR_FILENO);
-    // Close the original pipe file descriptors
-    if (command.pipe[0] != STDIN_FILENO) {
-        close(command.pipe[0]);
-    }
-    if (command.pipe[1] != STDOUT_FILENO) {
-        close(command.pipe[1]);
-    }
-    vector<char *> args;
-    for (const auto &arg : command.args) {
-        args.push_back(strdup(arg.c_str()));
-    }
-    args.push_back(nullptr);
-    if (execvp(args[0], args.data()) == -1 && errno == ENOENT) {
-        cerr << "Unknown command: [" << args[0] << "].\n";
-        // Free allocated memory before exiting
-        for (char *arg : args) {
-            free(arg);
-        }
-        exit(0);
-    }
-    return;
-}
-
-void initShell() {
-    clearenv();
-    setenv("PATH", "bin:.", 1);
-    signal(SIGCHLD, sigFork); // call wait() catch state
-}
-
-void update_pipeMap(unordered_map<int, pair<int, int>> &pipeMap) {
-    unordered_map<int, pair<int, int>> new_map;
-    for (const auto [key, value] : pipeMap) {
-        new_map.emplace(key - 1, value); // reduce pipeNum
-    }
-    pipeMap = new_map;
-}
-
-vector<Command> parseCommands(string input,
-                              unordered_map<int, pair<int, int>> &pipeMap) {
-    vector<Command> commands;
-    vector<string> command_args;
-    stringstream ss(input);
-    string arg;
-    while (getline(ss, arg, ' ')) {
-        if (arg[0] == '|' || arg[0] == '!' || arg[0] == '>') {
-            Command command;
-            command.args = command_args;
-            command_args.clear();
-
-            if (pipeMap.count(0)) { // pipe fd_in
-                command.pipe[0] = pipeMap[0].first;
-                command.pipe[1] = pipeMap[0].second;
-                pipeMap.erase(0);
+  public:
+    void createPipe(int pipe_id) {
+        array<int, 2> pipe_fds;
+        while (pipe(pipe_fds.data()) == -1) {
+            if (errno == EMFILE || errno == ENFILE) {
+                wait(nullptr);
             }
+        }
+        fcntl(pipe_fds[0], F_SETFD, FD_CLOEXEC);
+        fcntl(pipe_fds[1], F_SETFD, FD_CLOEXEC);
+        active_pipes.emplace(pipe_id, pipe_fds);
+    }
 
-            if (arg[0] == '>') {
-                string filename;
-                getline(ss, filename, ' ');
-                command.fd_out =
-                    open(filename.c_str(),
-                         O_CREAT | O_WRONLY | O_TRUNC | O_CLOEXEC, 0664);
+    bool hasPipe(int pipe_id) const { return active_pipes.count(pipe_id) > 0; }
+
+    array<int, 2> getPipe(int pipe_id) const {
+        return active_pipes.at(pipe_id);
+    }
+
+    void removePipe(int pipe_id) { active_pipes.erase(pipe_id); }
+
+    void shiftPipeNumbers() {
+        unordered_map<int, array<int, 2>> new_pipes;
+        for (const auto &[id, fds] : active_pipes) {
+            new_pipes.emplace(id - 1, fds);
+        }
+        active_pipes = move(new_pipes);
+    }
+};
+
+class CommandParser {
+    stringstream input_stream;
+    vector<string> current_args;
+    PipeManager &pipe_manager;
+
+  public:
+    CommandParser(const string &line, PipeManager &pm)
+        : input_stream(line), pipe_manager(pm) {}
+
+    void processCommands() {
+        string token;
+
+        while (input_stream >> token) {
+            if (isOperator(token[0])) {
+                executeCurrentCommand(token);
             } else {
-                int pipeNum = 0;
-                if (arg.size() > 1) {
-                    pipeNum = stoi(arg.substr(1));
-                }
-
-                if (pipeMap.count(pipeNum) == 0) {
-                    int pipe_fd[2];
-                    while (pipe(pipe_fd) == -1) {
-                        if (errno == EMFILE || errno == ENFILE) {
-                            wait(nullptr);
-                        }
-                    }
-                    pipeMap.emplace(pipeNum, make_pair(pipe_fd[0], pipe_fd[1]));
-                }
-                if (arg[0] == '|') {
-                    command.fd_out = pipeMap[pipeNum].second;
-                }
-
-                if (arg[0] == '!') { // pipe fd_out, fd_err
-                    command.fd_out = pipeMap[pipeNum].second;
-                    command.fd_err = pipeMap[pipeNum].second;
-                }
+                current_args.push_back(token);
             }
-            commands.push_back(command);
-            if (arg != "|") { // "|" piping without updating pipeMap
-                update_pipeMap(pipeMap);
-            }
+        }
+
+        executeRemainingCommand();
+    }
+
+  private:
+    bool isOperator(char c) const { return c == '|' || c == '!' || c == '>'; }
+
+    void executeCurrentCommand(const string &operator_token) {
+        ProcessExecutor::ProcessConfig config;
+        config.arguments = move(current_args);
+        current_args.clear();
+
+        setupInputPipe(config);
+        handleOperator(operator_token, config);
+
+        ProcessExecutor::run(config);
+
+        if (operator_token != "|") {
+            pipe_manager.shiftPipeNumbers();
+        }
+    }
+
+    void setupInputPipe(ProcessExecutor::ProcessConfig &config) {
+        if (pipe_manager.hasPipe(0)) {
+            config.input_pipe = pipe_manager.getPipe(0);
+            pipe_manager.removePipe(0);
+        }
+    }
+
+    void handleOperator(const string &op,
+                        ProcessExecutor::ProcessConfig &config) {
+        if (op[0] == '>') {
+            handleRedirection(config);
         } else {
-            command_args.push_back(arg);
+            handlePiping(op, config);
         }
     }
-    if (!command_args.empty()) { // parse last command
-        Command command;
-        command.args = command_args;
 
-        if (pipeMap.count(0)) { // pipe fd_in
-            command.pipe[0] = pipeMap[0].first;
-            command.pipe[1] = pipeMap[0].second;
-            pipeMap.erase(0);
-        }
-        commands.push_back(command);
-        update_pipeMap(pipeMap);
+    void handleRedirection(ProcessExecutor::ProcessConfig &config) {
+        string filename;
+        input_stream >> filename;
+        config.output_fd = open(filename.c_str(),
+                                O_CREAT | O_WRONLY | O_TRUNC | O_CLOEXEC, 0664);
     }
-    return commands;
-}
+
+    void handlePiping(const string &op,
+                      ProcessExecutor::ProcessConfig &config) {
+        int pipe_id = op.size() > 1 ? stoi(op.substr(1)) : 0;
+
+        if (!pipe_manager.hasPipe(pipe_id)) {
+            pipe_manager.createPipe(pipe_id);
+        }
+
+        auto pipe_fds = pipe_manager.getPipe(pipe_id);
+
+        if (op[0] == '|') {
+            config.output_fd = pipe_fds[1];
+        } else if (op[0] == '!') {
+            config.output_fd = pipe_fds[1];
+            config.error_fd = pipe_fds[1];
+        }
+    }
+
+    void executeRemainingCommand() {
+        if (!current_args.empty()) {
+            ProcessExecutor::ProcessConfig config;
+            config.arguments = move(current_args);
+
+            if (pipe_manager.hasPipe(0)) {
+                config.input_pipe = pipe_manager.getPipe(0);
+                pipe_manager.removePipe(0);
+            }
+
+            ProcessExecutor::run(config);
+            pipe_manager.shiftPipeNumbers();
+        }
+    }
+};
 
 int main() {
-    initShell();
+    signal(SIGCHLD, SIG_IGN);
+    setenv("PATH", "bin:.", 1);
 
-    unordered_map<int, pair<int, int>> pipeMap; // 0: read, 1: write
+    PipeManager pipe_manager;
+
     while (true) {
-        string input;
         cout << "% ";
+        string input;
         getline(cin, input);
-        if (input.empty()) {
+
+        if (input.empty())
             continue;
-        }
-        vector<Command> commands = parseCommands(input, pipeMap);
-        for (auto &command : commands) {
-            executeCommand(command);
-        }
+
+        CommandParser parser(input, pipe_manager);
+        parser.processCommands();
     }
+
     return 0;
 }
